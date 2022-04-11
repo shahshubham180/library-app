a0e6424052f12eaead0bad85ad00b8f49c84a62a
shubham shah <sshah4@vmware.com>
2022-04-11 15:14:42 +0530

    updateApp

--- src/main/java/com/springboot/tutorials/libraryapp/LibraryAppApplication.java
+++ src/main/java/com/springboot/tutorials/libraryapp/LibraryAppApplication.java
@@ -2,12 +2,26 @@ package com.springboot.tutorials.libraryapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.springboot.tutorials.libraryapp.customer.utils.CustomerUtils;
 
 @SpringBootApplication
 public class LibraryAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryAppApplication.class, args);
+	}
+	
+	@Bean
+	public CustomerUtils getCustomerUtils() {
+		return new CustomerUtils();
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
